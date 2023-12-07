fdc4466cbf7f6001203fc5dfc3245c51885cac9e
theAcer <samsonnjogu@gmail.com>
2023-12-07 22:27:33 +0300

    initial commit

---
+++ wej_core/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from wej_core.users.models import User
+from wej_core.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
